7426b89bceb81305c423e7f5caafd2b0adfe24f3
Jan Klempar <jan.klempar@vista.com>
2025-05-09 11:01:50 +0200

    added singles and pair combination. TODO for next: Refactor dictionaries for more suitable objects

--- PedroKatas/YahtzeeShould.cs
+++ PedroKatas/YahtzeeShould.cs
@@ -2,13 +2,19 @@ using Xunit;
 using FluentAssertions;
 using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace PedroKatas;
 
 public enum CATEGORY
 {
     FOURs,
-    TWOs
+    TWOs,
+    THREES,
+    FIVES,
+    SIXES,
+    ONES,
+    PAIR
 }
 
 public class YahtzeeShould
@@ -47,6 +53,61 @@ public class YahtzeeShould
                     { CATEGORY.TWOs, 0 },
                     { CATEGORY.FOURs, 8 }
                 }
+            },
+            new object[]
+            {
+                "Honza2",
+                new List<int> { 3, 3, 3, 3, 1 },
+                CATEGORY.THREES,
+                new Dictionary<CATEGORY, int>
+                {
+                    { CATEGORY.TWOs, 0 },
+                    { CATEGORY.THREES, 12 }
+                }
+            },
+            new object[]
+            {
+                "Satish2",
+                new List<int> { 5, 5, 5, 5, 5 },
+                CATEGORY.FIVES,
+                new Dictionary<CATEGORY, int>
+                {
+                    { CATEGORY.TWOs, 0 },
+                    { CATEGORY.FIVES, 25 }
+                }
+            },
+            new object[]
+            {
+                "Honza3",
+                new List<int> { 1, 2, 3, 4, 6 },
+                CATEGORY.SIXES,
+                new Dictionary<CATEGORY, int>
+                {
+                    { CATEGORY.TWOs, 0 },
+                    { CATEGORY.SIXES, 6 }
+                }
+            },
+            new object[]
+            {
+                "Satish3",
+                new List<int> { 1, 2, 3, 4, 5 },
+                CATEGORY.ONES,
+                new Dictionary<CATEGORY, int>
+                {
+                    { CATEGORY.TWOs, 0 },
+                    { CATEGORY.ONES, 1 }
+                }
+            },
+            new object[]
+            {
+                "HonzaPair",
+                new List<int> {4, 4},
+                CATEGORY.PAIR,
+                new Dictionary<CATEGORY, int>
+                {
+                    { CATEGORY.TWOs, 0 },
+                    { CATEGORY.PAIR, 8 }
+                }
             }
         };
 }
@@ -68,6 +129,42 @@ public class YatzeeScoringSystem
                 { CATEGORY.TWOs, 0 },
                 { CATEGORY.FOURs, 0 }
             }
+            
+        },
+        {
+            "Honza2", new Dictionary<CATEGORY, int>
+            {
+                { CATEGORY.TWOs, 0 },
+                { CATEGORY.THREES, 0 }
+            }
+        },
+        {
+            "Satish2", new Dictionary<CATEGORY, int>
+            {
+                { CATEGORY.TWOs, 0 },
+                { CATEGORY.FIVES, 0 }
+            }
+        },
+        {
+            "Honza3", new Dictionary<CATEGORY, int>
+            {
+                { CATEGORY.TWOs, 0 },
+                { CATEGORY.SIXES, 0 }
+            }
+        },
+        {
+            "Satish3", new Dictionary<CATEGORY, int>
+            {
+                { CATEGORY.TWOs, 0 },
+                { CATEGORY.ONES, 0 }
+            }
+        },
+        {
+            "HonzaPair", new Dictionary<CATEGORY, int>
+            {
+                { CATEGORY.TWOs, 0 },
+                { CATEGORY.PAIR, 0 }
+            }
         }
     };
 
@@ -75,10 +172,49 @@ public class YatzeeScoringSystem
     {
         int score = 0;
 
+        // combinations
+        if (CATEGORY.PAIR == expectedCATEGORY)
+        {
+            // var groupedRolls = chosenRolls.GroupBy(x => x)
+            //     .Where(g => g.Count() >= 2)
+            //     .Select(g => g.Key)
+            //     .ToList();
+
+            int numberOfAppearences = 0;
+            int chosenPairDie = 0;
+
+            for (var dieIndex = 0; dieIndex < chosenRolls.Count; dieIndex++)
+            {
+                for (var dieIndexInner = 0; dieIndexInner < chosenRolls.Count; dieIndexInner++)
+                {
+                    if (dieIndex == dieIndexInner)
+                    {
+                        continue;
+                    }
+                    if (chosenRolls[dieIndex] == chosenRolls[dieIndexInner])
+                    {
+                        numberOfAppearences++;
+                        chosenPairDie = chosenRolls[dieIndex];
+                    }
+                }
+            }
+            if (numberOfAppearences == 2)
+            {
+                score = chosenPairDie * 2;
+            }
+            scordCard[name][expectedCATEGORY] = score;
+            return scordCard[name]; 
+        }
+        
+        // singles
         foreach (var die in chosenRolls)
         {
             if ((expectedCATEGORY == CATEGORY.TWOs && die == 2) ||
-                (expectedCATEGORY == CATEGORY.FOURs && die == 4))
+                (expectedCATEGORY == CATEGORY.FOURs && die == 4) ||
+                (expectedCATEGORY == CATEGORY.THREES && die == 3) ||
+                (expectedCATEGORY == CATEGORY.FIVES && die == 5) ||
+                (expectedCATEGORY == CATEGORY.SIXES && die == 6) ||
+                (expectedCATEGORY == CATEGORY.ONES && die == 1))
             {
                 score += die;
             }
